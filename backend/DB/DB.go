1b34f7a1925ecfe5a4cad296c0776762bb39b437
vski5 <2643380185@qq.com>
2022-07-27 15:41:17 +0800

    no message

--- backend/DB/DB.go
+++ backend/DB/DB.go
@@ -11,6 +11,7 @@ func TouchSql() {
 	dsn := "user:password@tcp(127.0.0.1:3306)/dbname"
 	// 创建数据库连接,返回一个*sql.DB类型的数据库连接,这个连接可以用于执行SQL语句
 	//指定数据库mysql,指定数据源dsn
+	//Open函数可能只是验证其参数格式是否正确，实际上并不创建与数据库的连接。如果要检查数据源的名称是否真实有效，应该调用Ping方法。
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
