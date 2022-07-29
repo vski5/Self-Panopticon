cb25c876a63d6a758575ee24b3916bf4ffb5186e
vski5 <2643380185@qq.com>
2022-07-29 17:43:00 +0800

    添加todolist的原型，还有，点击触发事件的组件

--- front/react-app/src/App.js
+++ front/react-app/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+
+  ])
+  const [newName, setNewName] = useState('write name')
+  const [newNumber, setNewNumber] = useState('write number')
+  const changeperson =(event)=>{
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const personObject = {
+      name: 'write name' ,
+      number : 'write number'
+    }
+  
+    setPersons(persons.concat(personObject)) 
+    setNewName('')
+    setNewNumber('')
+  } 
+  const handleNameChange = (event) => {   
+    console.log(event.target.value)    
+    setNewName(event.target.value)     
+    }
+  const handleNumberChange = (event) => {   
+      console.log(event.target.value)    
+      setNewNumber(event.target.value)     
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+        Name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          Number: <input value={newNumber} onChange={handleNumberChange}/>
+          </div>
+        <div>
+        <button type="submit" onClick={changeperson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+      {persons.map(person => <ul>{person.name}  :  {person.number}</ul>)}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
