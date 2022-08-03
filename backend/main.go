dcd42b8eceabc8b09addc66eb9e494b3d9b24dbd
vski5 <2643380185@qq.com>
2022-08-03 11:16:43 +0800

    无伤大雅的小文字改动

--- backend/main.go
+++ backend/main.go
@@ -23,12 +23,12 @@ import (
 func main() {
 	//加载配置文件，判断setting是否出错
 	if err := settings.Init(); err != nil {
-		fmt.Printf("setting加载配置文件错了,err:%v\n", err)
+		fmt.Printf("setting加载配置文件错误,err:%v\n", err)
 		return
 	}
 	//初始化日志
 	if err := logger.Init(); err != nil {
-		fmt.Printf("logger初始化日志错了,err:%v\n", err)
+		fmt.Printf("logger初始化日志错误,err:%v\n", err)
 		return
 	}
 
@@ -39,7 +39,7 @@ func main() {
 
 	//初始化mysql
 	if err := mysql.Init(); err != nil {
-		fmt.Printf("mysql初始化日志错了,err:%v\n", err)
+		fmt.Printf("mysql初始化日志错误,err:%v\n", err)
 		return
 	}
 	//延迟注册，记得关mysql
@@ -47,7 +47,7 @@ func main() {
 
 	//初始化redis连接
 	if err := redis.Init(); err != nil {
-		fmt.Printf("redis初始化日志错了,err:%v\n", err)
+		fmt.Printf("redis初始化日志错误,err:%v\n", err)
 		return
 	}
 	//	记得关
